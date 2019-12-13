b4a2c86f1d6aa5957bac325914fa546b9922a7b5
Carlos Loaiza Hernandez <carlosloaiza21@hotmail.com>
2019-12-13 12:17:07 -0600

    URI Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let DB;
 if(process.env.NODE_ENV === 'dev') {
   DB ='mongodb://localhost:27017/cafe'
 } else {
-  DB ='mongodb+srv://cloaiza21:ncmaya01052011@cluster0-1labi.mongodb.net/cafe?retryWrites=true&w=majority'
+  DB = process.env.MONGO_URI
 }
 
 process.env.urlDB = DB;
